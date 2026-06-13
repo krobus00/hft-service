f1a43e457245f2a9269df7050a93330d2c4f8149
krobus00 <michaelputeraw@gmail.com>
2026-06-14 02:31:51 +0700

    fix(infra): fix db init command

--- tools/postgre/init-multiple-dbs.sh
+++ tools/postgre/init-multiple-dbs.sh
@@ -2,12 +2,9 @@
 set -e
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
-  IFS=',' read -r -a dbs <<EOF
-$POSTGRES_MULTIPLE_DATABASES
-EOF
-
-  for db in "${dbs[@]}"; do
+  echo "$POSTGRES_MULTIPLE_DATABASES" | tr ',' '\n' | while IFS= read -r db; do
     db="$(echo "$db" | xargs)" # trim spaces
+    [ -n "$db" ] || continue
     echo "Creating database: $db"
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres \
       -c "CREATE DATABASE \"$db\";"
